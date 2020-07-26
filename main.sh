1882ff297ff77740b8cc17a3cfcd8cbf731a7c84
selfup <boudinot.regis@yahoo.com>
2020-07-26 17:53:50 -0600

    formatting

--- main.sh
+++ main.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-pages='1 2 3 4 5'
+pages='1 2 3 4 5 6 7 8 9 10'
 
 echo 'pulling all repos..'
 
 for page in $pages
 do
-  curl -s https://api.github.com/users/selfup/repos?page=$page \
+  echo "page $page"
+
+  curl -s "https://api.github.com/users/selfup/repos?page=${page}" \
     | perl -ne 'print "$1\n" if (/"ssh_url": "([^"]+)/)' \
     | xargs -n 1 git clone
 done
 
 echo 'done!'
-
